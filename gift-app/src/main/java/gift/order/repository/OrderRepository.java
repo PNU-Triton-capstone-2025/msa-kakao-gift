8a93c4bdd9804b2ae923699bfd52c5b6106aa0a6
sul1074 <sul107485@gmail.com>
2025-08-20 11:21:57 +0900

    필요없는 파일들 제거

--- gift-app/src/main/java/gift/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package gift.order.repository;
-
-import gift.order.domain.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- gift-app/src/main/java/gift/order/service/OrderService.java
+++ gift-app/src/main/java/gift/order/service/OrderService.java
@@ -1,19 +1,9 @@
 package gift.order.service;
 
-import gift.auth.oauth.event.KakaoOrderCompletedEvent;
-import gift.member.domain.Member;
 import gift.member.dto.MemberTokenRequest;
-import gift.member.repository.MemberRepository;
-import gift.order.domain.Order;
 import gift.order.dto.OrderRequestDto;
 import gift.order.dto.OrderResponseDto;
-import gift.order.repository.OrderRepository;
-import gift.product.domain.ProductOption;
-import gift.product.repository.ProductOptionRepository;
-import gift.wish.repository.WishRepository;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClient;
 
 @Service
