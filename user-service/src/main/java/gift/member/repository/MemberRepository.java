44c6841a3824f0718a847df0026fdd7fbe9f3aa2
sul1074 <sul107485@gmail.com>
2025-08-25 14:31:54 +0900

    user-service 파일 1차 이동

---
+++ user-service/src/main/java/gift/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package gift.member.repository;
+
+import gift.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByKakaoId(Long kakaoId);
+}
